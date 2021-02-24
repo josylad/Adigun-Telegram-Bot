5cdc2d5acd50e3eac39ca7307d329c4c1cb5e603
Joseph Adediji <josylado@gmail.com>
2021-02-24 18:36:26 +0100

    Update bot

--- bot.py
+++ bot.py
@@ -23,11 +23,19 @@ bot = telebot.TeleBot(TOKEN, parse_mode="HTML") # You can set parse_mode by defa
 @bot.message_handler(commands=['start'])
 def send_welcome(m):
     user_markup = types.ReplyKeyboardMarkup(True, True)
-    user_markup.row(question1, question2)
-    user_markup.row(question3, question4)
-    user_markup.row(question5, question6)
+    user_markup.row(question1,question2 )
+    user_markup.row(question3)
+    user_markup.row(question4)
+    user_markup.row(question5)
+    user_markup.row(question6)
+    user_markup.row(question7)
+    user_markup.row(question8)
+    user_markup.row(question9)
+    user_markup.row(question10)
+    user_markup.row(question11)
+    user_markup.row(question12)
     cid = m.chat.id
-    line1 = 'Hello, I\'m Alexandra 🤖! Press any button below to interact with me. You will love using me to get Blockchain information.'
+    line1 = 'Hello, I\'m Adigun 🤖! Press any button below to interact with me. You will love using me to get Blockchain information.'
     msg = line1
     bot.send_message(cid, msg, reply_markup=user_markup)
 
@@ -40,6 +48,9 @@ def main_menu(m):
     user_markup.row(question1, question2)
     user_markup.row(question3, question4)
     user_markup.row(question5, question6)
+    user_markup.row(question7, question8)
+    user_markup.row(question9, question10)
+    user_markup.row(question11, question12)
     cid = m.chat.id
     user_msg = 'Welcome to the main menu.\n\n'
     bot.send_message(cid, user_msg, reply_markup=user_markup)
@@ -82,6 +93,36 @@ def send_answer(m):
 @bot.message_handler(regexp='^How does a blockchain work$')
 def send_answer(m):
     user_msg = answer6
+    bot.reply_to(m, user_msg)
+
+@bot.message_handler(regexp='^What is a blockchain application$')
+def send_answer(m):
+    user_msg = answer7
+    bot.reply_to(m, user_msg)
+
+@bot.message_handler(regexp='^What are the benefits of blockchain technology$')
+def send_answer(m):
+    user_msg = answer8
+    bot.reply_to(m, user_msg)
+
+@bot.message_handler(regexp='^What is the blockchain revolution$')
+def send_answer(m):
+    user_msg = answer9
+    bot.reply_to(m, user_msg)
+
+@bot.message_handler(regexp='^What is decentralized finance$')
+def send_answer(m):
+    user_msg = answer10
+    bot.reply_to(m, user_msg)
+
+@bot.message_handler(regexp='^What is a block in a blockchain$')
+def send_answer(m):
+    user_msg = answer11
+    bot.reply_to(m, user_msg)
+
+@bot.message_handler(regexp='^What is a blockchain wallet$')
+def send_answer(m):
+    user_msg = answer12
     bot.reply_to(m, user_msg)
 
 # Upon calling this function, TeleBot starts polling the Telegram servers for new messages.
