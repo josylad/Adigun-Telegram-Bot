3adc88d4ff0b2eef67cc773ca0344094d89581ab
Joseph Adediji <josylado@gmail.com>
2021-02-24 17:19:34 +0100

    Add bot response file

---
+++ replies.py
@@ -0,0 +1,52 @@
+question1 = "What is Blockchain"
+answer1 = "A blockchain, originally block chain, is a growing list of records, called blocks, that are linked using cryptography. Each block contains a cryptographic hash of the previous block, a timestamp, and transaction data. By design, a blockchain is resistant to modification of its data."
+
+question2 = "What is blockchain software"
+answer2 = '''Blockchain software is like any other software. The first of its kind was Bitcoin, which was released as open source software, making it available to anyone to use or change. 
+            There are a wide variety of efforts across the blockchain ecosystem to improve upon Bitcoin’s original software. 
+            '''
+
+question3 ="What Is A Private Blockchain"
+answer3 = "Private blockchains are deployed either within an organization or shared among a known group of participants. They can be limited to a predefined set of participants. In this case, no one else can access them or the data residing in them. "
+
+question4 = "What Is A Smart Contract"
+answer4 = "A Smart Contract is code that is deployed to the blockchain. Each smart contract contains code that can have a predefined set of inputs. Smart contracts can also store data. Following the distributed model of the blockchain, smart contracts run on every node in this technology, and each contract’s data is stored in every node. This data can be queried at any time. "
+
+question5 = "What is Mining"
+answer5 = "Mining is used a proof of work for participants in the blockchain. Whenever a block of transactions is to be agreed, every participating node attempts to ‘mine’ the block (a mathematical algorithmic process that requires extensive CPU capacity). In public blockchains successful mining is rewarded with a cryptocurrency token."
+
+question6 = "How does a blockchain work"
+answer6 = "When a digital transaction occurs in a blockchain network, it is grouped together in a cryptographically-secure “block” with other transactions that have occurred in the same time frame. The block is then broadcast to the network. \n A blockchain network is comprised of nodes or participants who validate and relay transaction information.\n The block of transactions is verified by participants called miners, who use computing power to solve a cryptographic puzzle and validate the block of transactions."
+
+question7 = "What is a blockchain application"
+answer7 = "Blockchain applications are comparable to conventional software applications, except they implement a decentralized architecture and cryptoeconomic systems to increase security, foster trust, tokenize assets, and design new network incentives. Here are over 90 Ethereum apps that are currently being used across the Ethereum blockchain ecosystem, from prediction markets to smart legal agreements."
+
+question8 = "What are the benefits of blockchain technology"
+answer8 = "Blockchain technology has a wide variety of benefits, for both global enterprises and local communities. The most commonly cited benefits of a blockchain are trusted data coordination, attack-resistance, shared IT infrastructure, tokenization, and built-in incentivization."
+
+question9 = "What is the blockchain revolution"
+answer9 = "Blockchain is considered a disruptive technology because of its ability to safeguard personal information, reduce intermediaries, unlock digital assets, and potentially open up the global economy to millions more participants. Sometimes called the Trust Machine, blockchain technology is bringing transparency and security to digital networks across countless industries. In many ways, the blockchain revolution can be considered a revolution in trust."
+
+question10 = "What is decentralized finance (DeFi)"
+answer10 = '''Decentralized finance—often called DeFi or open finance—refers to the economic paradigm shift enabled by decentralized technologies,
+ particularly blockchain networks. DeFi signals the shift from a historically centralized and closed financial system 
+ toward a universally accessible economy that is based on open protocols that are interoperable, programmable,
+  and composable. From streamlined and secure payment networks to automated loans to USD-pegged stablecoins, 
+  decentralized finance has emerged as one of the most active sectors in the blockchain space. Some of the defining 
+  factors of a DeFi application include permissionless architecture (anyone can participate), transparent and auditable code,
+   and interoperability with other DeFi products. DeFi Score offers a single, consistently comparable value for measuring 
+   DeFi platform risk.'''
+
+question11 = "What is a block in a blockchain"
+answer11 = ''' 
+            The “block” in a blockchain refers to a block of transactions that has been broadcast to the network. 
+            The “chain” refers to a string of these blocks. When a new block of transactions is validated by the network, 
+            it is attached to the end of an existing chain. This chain of blocks is an ever-growing ledger of transactions 
+            that the network has validated. We call this single, agreed-upon history of transactions a blockchain. 
+            Only one block can exist at a given chain height. There are several ways to add new blocks to an existing chain. 
+            These are often termed “proofs,” i.e. Proof of Work (PoW), Proof of Stake (PoS), and Proof of Authority (PoA). 
+            All involve cryptographic algorithms with varying degrees of complexity.
+            '''
+
+question12 = "What is a blockchain wallet"
+answer12 = "A blockchain wallet contains the public key for others to transfer cryptocurrency to your address and the private key so you can securely access your own digital assets. A blockchain wallet usually accompanies node hosting and stores cryptocurrencies on your computer. The safest place for storing digital assets is offline, what is often called 'cold storage.'"
